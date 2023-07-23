df2fdea10b0283362a0c93487eda124901ace41f
amberredmond <n01456296@unf.edu>
2023-07-22 21:44:30 -0400

    volunteer module added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vms';
+  userName = 'webwarriors';
+  password = 'fall2023';
+  welcome = 'Welcome to the volunteer management system!';
+  
+  auth(user: string, pass: string) : boolean{
+      if(this.userName == user && this.password == pass){
+        return true;
+      } else {
+        return false;
+      }
+  }
+  
+  message() : void{
+    console.log("Message to the console.");
+  }
 }
---
+++ src/app/volunteer/volunteer.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VolunteerComponent } from './volunteer.component';
+
+
+
+@NgModule({
+  declarations: [
+    VolunteerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class VolunteerModule { 
+  
+}
